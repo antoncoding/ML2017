8a6201eb682ebd1d0195d8b1d01a1ec409454aef
Anton Cheng <b03705027@ntu.edu.tw>
2017-02-23 22:45:35 +0800

    read different dimensions matrix

--- hw0/Q1.py
+++ hw0/Q1.py
@@ -1,19 +1,29 @@
 import numpy as np
 import sys
-
+#
 f1 = sys.argv[1]
 f2 = sys.argv[2]
 
-with open(f1,mode='r') as f1:
+num_lines_A = sum(1 for line in open(f1))
+num_lines_B = sum(1 for line in open(f2))
+
+with open(f1) as f1:
     dataA = f1.read()
-    matrix_A = np.matrix(dataA).reshape(1,50)
+    num_cols_A = len(dataA)//(num_lines_A *2)
 
 with open(f2, mode='r') as f2:
     dataB = f2.read()
-    matrix_B = np.matrix(dataB).reshape(50,10)
+    num_cols_B = len(dataB)//(num_lines_B *2)
 
+matrix_A = np.matrix(dataA).reshape(num_lines_A, num_cols_A)
+matrix_B = np.matrix(dataB).reshape(num_lines_B, num_cols_B)
 matrix_C = matrix_A * matrix_B
-matrix_C.sort()
+print_list = []
 f = open('ans_one.txt','w')
-for i in range(0,matrix_C.shape[1]):
-    print(matrix_C.item(i), file=f)
+for j in range(0,num_cols_B):
+    for i in range(0,num_lines_A):
+        print_list.append(matrix_C[i,j])
+
+print_list.sort()
+for item in print_list:
+    print(item, file=f)
